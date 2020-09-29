236a6da0723baadf6db9afa41086a329ba8d6f82
Aaron Kessler <jstask@me.com>
2020-09-29 19:16:41 +0200

    require plugin WP API Menus

--- functions.php
+++ functions.php
@@ -13,3 +13,53 @@ function headlesswp_remove_toolbar_node($wp_admin_bar) {
 	
 }
 add_action('admin_bar_menu', 'headlesswp_remove_toolbar_node', 999);
+
+/**
+* Check for Plugins
+* @see http://tgmpluginactivation.com/configuration/ for detailed documentation.
+*
+* @package    TGM-Plugin-Activation
+* @subpackage Example
+* @version    2.6.1 for parent theme Headless-Wordpress
+* @author     Thomas Griffin, Gary Jones, Juliette Reinders Folmer
+* @copyright  Copyright (c) 2011, Thomas Griffin
+* @license    http://opensource.org/licenses/gpl-2.0.php GPL v2 or later
+* @link       https://github.com/TGMPA/TGM-Plugin-Activation
+*/
+
+require_once get_template_directory() . '/includes/class-tgm-plugin-activation.php';
+add_action( 'tgmpa_register', 'headlesswp_register_required_plugins' );
+function headlesswp_register_required_plugins() {
+	/*
+	 * Array of plugin arrays. Required keys are name and slug.
+	 * If the source is NOT from the .org repo, then source is also required.
+	 */
+	$plugins = array(
+
+		// This is an example of how to include a plugin from the WordPress Plugin Repository.
+		array(
+			'name'      => 'WP API Menus',
+			'slug'      => 'wp-api-menus',
+			'required'  => true,
+		)
+	);
+
+	/*
+	 * Array of configuration settings. Amend each line as needed.
+	 * Only uncomment the strings in the config array if you want to customize the strings.
+	 */
+	$config = array(
+		'id'           => 'headlesswordpress',                 // Unique ID for hashing notices for multiple instances of TGMPA.
+		'default_path' => '',                      // Default absolute path to bundled plugins.
+		'menu'         => 'tgmpa-install-plugins', // Menu slug.
+		'parent_slug'  => 'themes.php',            // Parent menu slug.
+		'capability'   => 'edit_theme_options',    // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
+		'has_notices'  => true,                    // Show admin notices or not.
+		'dismissable'  => false,                    // If false, a user cannot dismiss the nag message.
+		'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
+		'is_automatic' => true,                   // Automatically activate plugins after installation or not.
+		'message'      => '',                      // Message to output right before the plugins table.
+	);
+
+	tgmpa( $plugins, $config );
+}
