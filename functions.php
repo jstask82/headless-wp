410827641351358462cfe8827eaf8d1e751faf9f
Aaron Kessler <jstask@me.com>
2020-09-29 17:34:04 +0200

    removed comment clutter

--- functions.php
+++ functions.php
@@ -3,10 +3,8 @@ add_theme_support('custom-logo');
 add_theme_support('menus');
 
 // remove toolbar items
-// https://digwp.com/2016/06/remove-toolbar-items/
 function headlesswp_remove_toolbar_node($wp_admin_bar) {
 	
-	// replace 'updraft_admin_node' with your node id
   $wp_admin_bar->remove_node('wp-logo');
   $wp_admin_bar->remove_node('customize');
   $wp_admin_bar->remove_node('comments');
