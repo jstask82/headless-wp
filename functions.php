e7d36dda21d1230ecdebc74e99601181450ca619
Aaron Kessler <jstask@me.com>
2020-09-29 17:31:57 +0200

    basic actionbar support

--- functions.php
+++ functions.php
@@ -1,3 +1,17 @@
 <?php
 add_theme_support('custom-logo');
 add_theme_support('menus');
+
+// remove toolbar items
+// https://digwp.com/2016/06/remove-toolbar-items/
+function headlesswp_remove_toolbar_node($wp_admin_bar) {
+	
+	// replace 'updraft_admin_node' with your node id
+  $wp_admin_bar->remove_node('wp-logo');
+  $wp_admin_bar->remove_node('customize');
+  $wp_admin_bar->remove_node('comments');
+  $wp_admin_bar->remove_node('edit');
+  $wp_admin_bar->remove_node('search');
+	
+}
+add_action('admin_bar_menu', 'headlesswp_remove_toolbar_node', 999);
--- index.php
+++ index.php
@@ -1,1 +1,4 @@
+<iframe src="http://localhost:8000/"  width="100%" height="100%" style="border:none;"></iframe>
 <?php
+  wp_footer();
+?>
