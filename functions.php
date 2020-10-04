6b46136d0e065d8832afd1bd16f892a76843157b
Aaron Kessler <jstask@me.com>
2020-10-04 19:34:53 +0200

    tumbnail support, more rest plugins

--- functions.php
+++ functions.php
@@ -1,6 +1,25 @@
 <?php
 add_theme_support('custom-logo');
 add_theme_support('menus');
+add_theme_support('post-thumbnails');
+
+//Add custom post types
+function headlesswp_custom_post_types(){
+  register_post_type('portfoilo',[
+    'labels' =>[
+      'name' => __('Portfolio'),
+      'singular_name' => __('Portfolio')
+    ],
+    'public' => true,
+    'show_in_admin_bar' => true,
+    'show_in_rest' => true,
+    'menu_position' => 5,
+    'menu_icon' => 'dashicons-pressthis',
+    'supports' => ['title', 'thumbnail', 'editor', 'excerpt']
+    ]);
+}
+
+add_action( 'init', 'headlesswp_custom_post_types' );
 
 // remove toolbar items
 function headlesswp_remove_toolbar_node($wp_admin_bar) {
@@ -34,32 +53,44 @@ function headlesswp_register_required_plugins() {
 	 * Array of plugin arrays. Required keys are name and slug.
 	 * If the source is NOT from the .org repo, then source is also required.
 	 */
-	$plugins = array(
+	$plugins = [
 
 		// This is an example of how to include a plugin from the WordPress Plugin Repository.
-		array(
+		[
 			'name'      => 'WP API Menus',
 			'slug'      => 'wp-api-menus',
 			'required'  => true,
-		)
-	);
+		],
+		[
+			'name'      => 'WP REST API favicon',
+			'slug'      => 'wp-rest-api-favicon',
+			'source'    => 'https://github.com/jstask82/wp-rest-api-favicon/archive/master.zip',
+			'required'  => true,
+		],
+		[
+			'name'      => 'WP REST API logo',
+			'slug'      => 'wp-rest-api-logo',
+			'source'    => 'https://github.com/jstask82/wp-rest-api-logo/archive/master.zip',
+			'required'  => true,
+		],
+	];
 
 	/*
 	 * Array of configuration settings. Amend each line as needed.
 	 * Only uncomment the strings in the config array if you want to customize the strings.
 	 */
-	$config = array(
-		'id'           => 'headlesswordpress',                 // Unique ID for hashing notices for multiple instances of TGMPA.
+	$config = [
+		'id'           => 'headlesswordpress',     // Unique ID for hashing notices for multiple instances of TGMPA.
 		'default_path' => '',                      // Default absolute path to bundled plugins.
 		'menu'         => 'tgmpa-install-plugins', // Menu slug.
 		'parent_slug'  => 'themes.php',            // Parent menu slug.
 		'capability'   => 'edit_theme_options',    // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
 		'has_notices'  => true,                    // Show admin notices or not.
-		'dismissable'  => false,                    // If false, a user cannot dismiss the nag message.
+		'dismissable'  => false,                   // If false, a user cannot dismiss the nag message.
 		'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
-		'is_automatic' => true,                   // Automatically activate plugins after installation or not.
+		'is_automatic' => true,                    // Automatically activate plugins after installation or not.
 		'message'      => '',                      // Message to output right before the plugins table.
-	);
+	];
 
 	tgmpa( $plugins, $config );
 }
